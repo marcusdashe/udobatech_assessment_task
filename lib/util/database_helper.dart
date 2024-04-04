f135b56715b3ae6de64a0fe2c6517ad49d740762
Marcus Dashe <marcus.dashe@totalsecureng.com>
2024-04-04 12:16:15 +0100

    luca: done implementing pow for flutter crud with mysql

--- lib/util/database_helper.dart
+++ lib/util/database_helper.dart
@@ -10,13 +10,5 @@ class DatabaseHelper {
     ); // Replace with your database name
     return await MySqlConnection.connect(settings);
   }
-
-  // Add methods for CRUD operations
-  // Future<List<BlogPost>> fetchPosts() async {
-  //   final conn = await getConnection();
-  //   final results = await conn.query('SELECT * FROM your_blog_table');
-  //   // return results.map((row) => BlogPost.fromJson(row)).toList();
-  //   return results.map((row) => BlogPost.fromJson(row.toMap())).toList();
-  // }
 }
 
